5e7dec8b39edaf9dec18eab48c85d0dafa460825
Nelson <nbritob@hotmail.com>
2021-03-16 16:45:09 +0000

    adds selenium usage to put gathered info into a google form

--- main.py
+++ main.py
@@ -24,15 +24,37 @@ for link in all_link_elements:
         all_links.append(href)
 
 all_address_elements = soup.select(".propertyCard-details address")
-
-all_addresses = [address.get_text().replace("\n", "") for address in all_address_elements]
-
+all_addresses = [address.get_text().replace("\n", "")
+                 for address in all_address_elements]
 
 all_price_elements = soup.select(".propertyCard-priceValue")
-all_prices = [price.get_text().strip() for price in all_price_elements if "£" in price.text]
+all_prices = [price.get_text().strip()
+              for price in all_price_elements if "£" in price.text]
 
 all_price_qualifier_elements = soup.select(".propertyCard-priceQualifier")
-all_qualifiers = [qualifier.get_text().strip() for qualifier in all_price_qualifier_elements]
+all_qualifiers = [qualifier.get_text().strip()
+                  for qualifier in all_price_qualifier_elements]
 
-all_prices_with_qualifiers = [' '.join(combination) for combination in zip(all_qualifiers, all_prices)]
-print(all_prices_with_qualifiers)
+all_prices_with_qualifiers = [
+    ' '.join(combination) for combination in zip(all_qualifiers, all_prices)]
+
+completed_property = ['. '.join(full_property) for full_property in zip(
+    all_addresses, all_prices_with_qualifiers, all_links)]
+
+
+chrome_driver_path = "/Users/nelsonbrito/Documents/drivers/chromedriver"
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+for n in range(len(all_links)):
+    driver.get("https://forms.gle/SL6bnBmE1zCgZLUs8")
+
+    time.sleep(2)
+    
+    property_answer = driver.find_element_by_xpath(
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit = driver.find_element_by_xpath(
+        '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span/span')
+
+    property_answer.send_keys(completed_property[n])
+    submit.click()
