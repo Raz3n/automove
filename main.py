96c33e983f048797c85f0c97528f80dd1f27e906
Nelson <nbritob@hotmail.com>
2021-03-16 14:38:12 +0000

    adds all link retrieval

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
     "Accept-Language": "en-GB,en;q=0.9,en-US;q=0.8,fr;q=0.7,es;q=0.6"
 }
+
+response = requests.get("https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E475&minBedrooms=2&maxPrice=300000&sortType=1&propertyTypes=&maxDaysSinceAdded=14&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=")
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+all_link_elements = soup.select(".propertyCard-details a")
+all_links = []
+
+for link in all_link_elements:
+    href = link["href"]
+    if "http" not in href:
+        all_links.append(f"https://rightmove.co.uk{href}")
+    else:
+        all_links.append(href)
+
+print(all_links)
